2d16d4181fd19f41ba705441b8b6241af362b02a
anttikar <antti.karhu@solita.fi>
2017-12-19 16:01:35 +0200

    Displaying 24 sprites at once

--- app.asm
+++ app.asm
@@ -12,6 +12,19 @@ IRQRASTER  = $D012
 CIA1IRQ    = $DC0D
 RASTERREG  = $D011
 
+SPR_ENABLE      = $D015 ; FLAGS FOR SPRITE ENABLING
+SPR_MSBX        = $D010 ; FLAGS TO REPRESENT X VALUES LARGER THAN 255
+SPR_COLORMODE   = $D01C ; FLAGS TO SET COLOR MODES (0 = HIGH RES/2-COLOR, 1 = MULTICOLOR/4-COLOR)
+SPR_COLOR0      = $D025 ; SHARED SPRITE COLOR 0
+SPR_COLOR1      = $D026 ; SHARED SPRITE COLOR 1
+
+SPR0_PTR        = $07F8 ; SPRITE 0 DATA POINTER
+SPR0_ADDR       = #$0D  ; SPRITE 0 POINTER VALUE
+SPR0_DATA       = $0340 ; SPRITE 0 DATA ADDRESS (POINTER VALUE * $40)
+SPR0_X          = $D000 ; SPRITE 0 X COORDINATE
+SPR0_Y          = $D001 ; SPRITE 0 Y COORDINATE
+SPR0_COLOR      = $D027 ; SPRITE 0 COLOR
+
 ; KERNAL ROUTINES
 IRQCONTINUE     = $EA81
 IRQFINISH       = $EA31
@@ -37,11 +50,101 @@ INIT
         LDA #%00000001 ; ENABLE RASTER INTERRUPTS ONLY AFTER SETUP
         STA IRQCTRL
 
+        ; SPRITES
+
+        ; ENABLE SPRITES
+        LDA #%11111111
+        STA SPR_ENABLE
+
+        ; SET COLOR MODES
+        LDA #%11111111
+        STA SPR_COLORMODE
+        
+        ; SET SPRITE SHARED COLORS
+        LDA #0
+        STA SPR_COLOR0
+   
+        ; SET SPRITE X
+        LDX #%00000000
+        STX SPR_MSBX
+        LDX #40
+        STX $D000
+        LDX #70
+        STX $D002
+        LDX #100
+        STX $D004
+        LDX #130
+        STX $D006
+        LDX #160
+        STX $D008
+        LDX #190
+        STX $D00A
+        LDX #220
+        STX $D00C
+        LDX #250
+        STX $D00E
+        
+        ; SET SPRITE POINTERS
+        LDA #$F8
+        STA $FA
+        LDA #$07
+        STA $FB
+        LDA SPR0_ADDR
+        LDY #0
+SETPTRS STA ($FA),Y
+        INY
+        CPY #16
+        BNE SETPTRS
+
+        ; LOAD SPRITE DATA
+        LDX #0
+LDSPR1  LDA SPRDATA,X
+        STA SPR0_DATA,X
+        INX
+        CPX #64
+        BNE LDSPR1
+
 MAIN    
         JMP MAIN
 
 IRQ0    
-        ; TODO
+        ; SET Y COORDINATES
+        LDA #$01
+        STA $FA
+        LDA #$D0
+        STA $FB
+        LDY #0
+        LDA #80
+SETY1   STA ($FA),Y
+        INY
+        INY
+        CPY #16
+        BNE SETY1
+
+        ; SET COLORS
+        LDA #$27
+        STA $FA
+        LDA #$D0
+        STA $FB
+        LDY #0
+        LDA #5
+        STA $FC
+SETCLR1 LDA $FC
+        STA ($FA),Y
+        INY
+        INC $FC
+        CPY #8
+        BNE SETCLR1
+
+        ; MOVE SPRITE
+        DEC $D000
+        DEC $D002
+        DEC $D004
+        DEC $D006
+        DEC $D008
+        DEC $D00A
+        DEC $D00C
+        DEC $D00E
 
         LDA #116 ; NEXT DO IRQ1 AT RASTER LINE 116
         STA IRQRASTER
@@ -55,7 +158,33 @@ IRQ0
         JMP IRQCONTINUE ; CONTINUE WITH IRQS
 
 IRQ1    
-        ; TODO
+        ; SET Y COORDINATES
+        LDA #$01
+        STA $FA
+        LDA #$D0
+        STA $FB
+        LDY #0
+        LDA #140
+SETY2   STA ($FA),Y
+        INY
+        INY
+        CPY #16
+        BNE SETY2
+        
+        ; SET COLORS
+        LDA #$27
+        STA $FA
+        LDA #$D0
+        STA $FB
+        LDY #0
+        LDA #2
+        STA $FC
+SETCLR2 LDA $FC
+        STA ($FA),Y
+        INY
+        INC $FC
+        CPY #8
+        BNE SETCLR2
 
         LDA #182 ; NEXT TO IRQ2 AT RASTER LINE 182
         STA IRQRASTER
@@ -69,7 +198,33 @@ IRQ1
         JMP IRQCONTINUE
 
 IRQ2    
-        ; TODO
+        ; SET Y COORDINATES
+        LDA #$01
+        STA $FA
+        LDA #$D0
+        STA $FB
+        LDY #0
+        LDA #200
+SETY3   STA ($FA),Y
+        INY
+        INY
+        CPY #16
+        BNE SETY3
+
+        ; SET COLORS
+        LDA #$27
+        STA $FA
+        LDA #$D0
+        STA $FB
+        LDY #0
+        LDA #8
+        STA $FC
+SETCLR3 LDA $FC
+        STA ($FA),Y
+        INY
+        INC $FC
+        CPY #8
+        BNE SETCLR3
 
         LDA #0 ; START AGAIN WITH IRQ0
         STA IRQRASTER
@@ -81,3 +236,26 @@ IRQ2
         ASL IRQFLAG
 
         JMP IRQFINISH ; LET MACHINE HANDLE OTHER IRQS
+
+SPRDATA BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$15,$40
+        BYTE $00,$7E,$91
+        BYTE $00,$7E,$91
+        BYTE $01,$FE,$A5
+        BYTE $15,$FE,$A4
+        BYTE $5A,$AA,$A9
+        BYTE $6A,$AA,$A9
+        BYTE $6A,$AA,$A9
+        BYTE $69,$AA,$99
+        BYTE $65,$6A,$55
+        BYTE $17,$55,$74
+        BYTE $05,$40,$54
+        BYTE $01,$00,$10
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00,$00,$00
+        BYTE $00
